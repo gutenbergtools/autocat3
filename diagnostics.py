41aedb60102a9ea8a5df5fa20effe855405d7f5b
eric <eric@hellman.net>
2019-07-29 17:45:12 -0400

    also check size of session cache

--- diagnostics.py
+++ diagnostics.py
@@ -11,8 +11,44 @@ Distributable under the GNU General Public License Version 3 or newer.
 import json
 import resource
 import sys
+from collections import Mapping, Container
+from sys import getsizeof
 from Page import Page
 from cherrypy.lib.sessions import RamSession
+
+def deep_getsizeof(o, ids):
+    """Find the memory footprint of a Python object
+
+    This is a recursive function that rills down a Python object graph
+    like a dictionary holding nested ditionaries with lists of lists
+    and tuples and sets.
+
+    The sys.getsizeof function does a shallow size of only. It counts each
+    object inside a container as pointer only regardless of how big it
+    really is.
+
+    :param o: the object
+    :param ids:
+    :return:
+    https://github.com/the-gigi/deep/blob/master/deeper.py
+    """
+    d = deep_getsizeof
+    if id(o) in ids:
+        return 0
+
+    r = getsizeof(o)
+    ids.add(id(o))
+
+    if isinstance(o, str):
+        return r
+
+    if isinstance(o, Mapping):
+        return r + sum(d(k, ids) + d(v, ids) for k, v in o.items())
+
+    if isinstance(o, Container):
+        return r + sum(d(x, ids) for x in o)
+
+    return r
 
 class DiagnosticsPage (Page):
     """ Python health. """
@@ -21,7 +57,9 @@ class DiagnosticsPage (Page):
         """ return stats. """
         stats = {}
         stats['sessions'] = len(RamSession.cache)
+        stats['sessions_storage'] = deep_getsizeof(RamSession.cache, set())
         stats['allocated_blocks'] = sys.getallocatedblocks()
-        stats['rusage_self'] = resource.rusage(resource.RUSAGE_SELF)
-        stats['rusage_children'] = resource.rusage(resource.RUSAGE_CHILDREN)
+        stats['rusage_self'] = resource.getrusage(resource.RUSAGE_SELF)
+        stats['rusage_children'] = resource.getrusage(resource.RUSAGE_CHILDREN)
         return json.dumps(stats)
+
