9e116d4b202c84cb5fdfdfa495d86bc98f1072d3
eric <eric@hellman.net>
2019-08-01 16:20:18 -0400

    add error page

---
+++ errors.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#  -*- mode: python; indent-tabs-mode: nil; -*- coding: utf-8 -*-
+
+from BaseSearcher import OpenSearch
+from Page import Page
+import Formatters
+
+class ErrorPage(Page):
+    
+    def __init__(self, status=500, message='undefined error'):
+        self.message = message
+        self.status = status
+
+    def index(self):
+        return Formatters.formatters['html'].render('error', self)
