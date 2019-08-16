213b0264cc0f918f11c2f9b0b38598949af83e24
eric <eric@hellman.net>
2019-08-16 13:18:02 -0400

    improved ramsession patch
    Ref: https://github.com/cherrypy/cherrypy/pull/1804

--- sessions.py
+++ sessions.py
@@ -8,24 +8,29 @@ class RamSession(cpRamSession):
 
         now = self.now()
         try:
-            for _id, (data, expiration_time) in list(self.cache.items()):
-                if expiration_time <= now:
-                    try:
-                        del self.cache[_id]
-                    except KeyError:
-                        pass
-                    try:
-                        if self.locks[_id].acquire(blocking=False):
-                            lock = self.locks.pop(_id)
-                            lock.release()
-                    except KeyError:
-                        pass
-
-        except RuntimeError:
+            cache_items_copy = self.cache.copy().items()
+        except RuntimeError as re:
             """Under heavy load, list(self.cache.items()) will occasionally raise this error
             for large session caches with message "dictionary changed size during iteration"
             Better to pause the cleanup than to let the cleanup thread die.
             """
+            cherrypy.log(f'Runtime Error happened while copying the cache entries: {re}')
+            cherrypy.log('Ref: https://github.com/cherrypy/cherrypy/pull/1804')
+            return
+
+        for _id, (data, expiration_time) in cache_items_copy:
+            if expiration_time <= now:
+                try:
+                    del self.cache[_id]
+                except KeyError:
+                    pass
+                try:
+                    if self.locks[_id].acquire(blocking=False):
+                        lock = self.locks.pop(_id)
+                        lock.release()
+                except KeyError:
+                    pass
+
 
             return 
 
