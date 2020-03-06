997808b7525bd8bbba35c339a7de935c8b411508
Marshall <363752922@qq.com>
2020-03-06 13:30:30 -0500

    doctype change for HTMLserializer

--- HTMLFormatter.py
+++ HTMLFormatter.py
@@ -129,9 +129,7 @@ class HTMLFormatter (XMLishFormatter):
     """ Produce HTML output. """
 
     CONTENT_TYPE = 'text/html; charset=UTF-8'
-    DOCTYPE      = ('html',
-                    '-//W3C//DTD XHTML+RDFa 1.0//EN',
-                    'http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd')
+    DOCTYPE      = 'html'
 
     def __init__ (self):
         super (HTMLFormatter, self).__init__ ()
@@ -195,9 +193,7 @@ class MobileFormatter (XMLishFormatter):
     """ Produce HTML output suitable for mobile devices. """
 
     CONTENT_TYPE = mt.xhtml + '; charset=UTF-8'
-    DOCTYPE      = ('html',
-                    '-//WAPFORUM//DTD XHTML Mobile 1.2//EN',
-                    'http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd')
+    DOCTYPE      = 'html'
 
     def __init__ (self):
         super (MobileFormatter, self).__init__ ()
