2cc0d90de1153370430e542add3c64093a725f5e
autocat <autocat@gutenberg1.ibiblio.org>
2020-05-29 14:28:19 -0400

    Revert "CoverPages.py changes"
    This reverts commit e5b4b23cc7c1bed1aad3b8a0eb8acfd8a94d4f88.
    
    Conflicts:
    	CoverPages.py

--- CoverPages.py
+++ CoverPages.py
@@ -18,7 +18,7 @@ import cherrypy
 import six
 import textwrap
 from libgutenberg import GutenbergGlobals as gg
-import re
+
 import BaseSearcher
 
 class CoverPages (object):
@@ -34,13 +34,28 @@ class CoverPages (object):
         cherrypy.response.headers['Content-Type'] = 'text/html; charset=utf-8'
         cherrypy.response.headers['Content-Language'] = 'en'
 
-        s = """<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" xml:base="http://www.gutenberg.org">-->
-<!--<head>
+        s = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+<!--"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" xml:base="http://www.gutenberg.org">
+<head>
 <title>Cover Flow</title>
-</head>-->
-<!--<body><div>-->"""
+<!--<style>
+.cover-thumb {
+        display: inline-block;
+        background-position: center;
+        background-repeat: no-repeat;
+}
+.cover-thumb-small {
+	width:  100px;
+	height: 150px;
+}
+.cover-thumb-medium {
+	width:  210px;
+	height: 310px;
+}
+</style>-->
+</head>
+<body><div>-->"""
 
         for row in rows:
             url = '/' + row.filename
@@ -54,6 +69,7 @@ class CoverPages (object):
 	    #Declaring this variable causes autocat service to fail
 	    #short_title = title
             title = title.replace ('"', '&quot;')
+<<<<<<< HEAD
 >>>>>>> parent of d672cc3... Changes for the cover title
             title_len = len(title)
             title = re.sub(r"\-+"," ",title)
@@ -71,10 +87,28 @@ class CoverPages (object):
         s += """<a href="{href}" title="{title}" target="_top"><div class="cover_image">
 		    <div class="cover_img"><img src="{url}" alt="{title}" title="{title}" draggable="false">
 		    </div><div class="cover_title"><h5>{title}</h5></div></div></a>\n""".format (
+=======
+            title_len=len(title)
+	    #title = re.sub (r'\s*\$[a-z].*', '', title)
+            title= title.splitlines()[0]	    
+            if(title_len>80):
+                title=textwrap.wrap(title,50)[0]
+
+
+            s += """<a href="{href}"
+                       title="{title}"
+                       target="_top"
+                       ><div class="cover-container"><img src="{url}" alt="{title}" title="{title}" draggable="false"><h5>{title}\n</h5></div></a>\n""".format (
+>>>>>>> parent of e5b4b23... CoverPages.py changes
                 url = url, href = href, title = title, size = size)
+<<<<<<< HEAD
         return (s + '<!--</div></body></html>-->\n').encode ('utf-8')
+=======
 
  
+        return (s + '</div></body></html>\n').encode ('utf-8')
+>>>>>>> 0253c4308e81c77c840eed00fd2df1fe998dc5f5
+
     def index (self, count, size, order, **kwargs):
         """ Internal help function. """
 
