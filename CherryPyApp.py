7ee64b4ead23e7a0af5bb403f1b2acbc97de42bf
Zach Rosario <zrosario301@gmail.com>
2026-01-30 11:42:15 -0500

    added support for cors headers in cherry py app opds route

--- CherryPyApp.py
+++ CherryPyApp.py
@@ -336,7 +336,12 @@ def main():
     cherrypy.tree.mount(OPDSFeed(), '/opds', {
         '/': {
             'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Access-Control-Allow-Origin', '*')],
+            'tools.response_headers.headers': [
+                ('Content-Type', 'application/opds+json'),
+                ('Access-Control-Allow-Origin', '*'),
+                ('Access-Control-Allow-Methods', 'GET'),
+                ('Access-Control-Allow-Headers', 'Accept, Content-Type'),
+            ],
         }
     })
 
