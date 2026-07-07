32abbfa95da8707b678f4c96a84eb7dec31ecd00
Zachary Rosario <zrosario301@gmail.com>
2026-07-07 19:47:20 -0400

    Added random sort order to bookshelf pages.

--- SearchPage.py
+++ SearchPage.py
@@ -218,7 +218,7 @@ class BookshelfSearchPage (SearchPage):
     def setup (self, os, sql):
         os.f_format_url = BaseSearcher.SearchUrlFormatter ('bookshelf')
         os.f_format_thumb_url = os.format_none
-        os.sort_orders = ('downloads', 'quantity', 'alpha', 'release_date')
+        os.sort_orders = ('downloads', 'quantity', 'alpha', 'release_date', 'random')
         os.icon = 'bookshelf'
         os.title_icon = 'bookshelf'
         os.class_ += 'navlink'
@@ -335,7 +335,7 @@ class BookshelfPage (SearchPage):
     """ bookshelf id => books on bookshelf """
 
     def setup (self, os, sql):
-        os.sort_orders = ('downloads', 'title', 'author', 'release_date')
+        os.sort_orders = ('downloads', 'title', 'author', 'release_date', 'random')
         os.title_icon = 'bookshelf'
         os.icon = 'book'
         os.class_ += 'booklink'
